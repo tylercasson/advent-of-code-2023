23257310de4aae71f8df168702ef3df39394e827
Tyler Casson <me@tylercasson.com>
2023-12-01 16:17:16 -0600

    refactor(day-01): minor part1 update

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,10 +1,9 @@
+use day_01::part1::run;
 use std::error::Error;
-
-use day_01::part1;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = include_str!("../../input/input1.txt");
-    let result = part1::run(input)?;
+    let result = run(input)?;
 
     println!("{:?}", result);
     Ok(())
--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,5 +1,13 @@
 use std::error::Error;
 
+pub fn extract_number_literal(input: &str) -> Result<char, Box<dyn Error>> {
+    if let Some(value) = input.chars().find(|char| char.is_numeric()) {
+        return Ok(value);
+    }
+    let message = format!("Error extracting number literal from input: {}", input);
+    Err(Box::from(message))
+}
+
 pub fn run(input: &str) -> Result<String, Box<dyn Error>> {
     let mut sum: u32 = 0;
     for line in input.lines() {
@@ -16,4 +24,20 @@ pub fn run(input: &str) -> Result<String, Box<dyn Error>> {
     }
 
     Ok(sum.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::run;
+
+    #[test]
+    fn part1_example_input() {
+        let expected = 142.to_string();
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!(expected, run(input).unwrap());
+    }
 }
