f1a2133a9b34496df90b70745adf4adff5e86cc3
Tyler Casson <me@tylercasson.com>
2023-12-01 16:35:48 -0600

    refactor(template): add more boilerplate

--- cargo-template-day/src/bin/part1.rs
+++ cargo-template-day/src/bin/part1.rs
@@ -1,3 +1,10 @@
-fn main() {
-    todo!("Implement part 1");
+use {{crate_name}}::part1::run;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = include_str!("../../input/input1.txt");
+    let result = run(input)?;
+
+    println!("{:?}", result);
+    Ok(())
 }
--- cargo-template-day/src/part1.rs
+++ cargo-template-day/src/part1.rs
@@ -1,1 +1,15 @@
+use std::error::Error;
 
+pub fn run(input: &str) -> Result<String, Box<dyn Error>> {
+    todo!("Implement part 1");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::run;
+
+    #[test]
+    fn part1_example_input() {
+        todo!("Test example input for part 1");
+    }
+}
