80e1688e139e20ed0bcd677d91e11cbc472f4599
Tyler Casson <me@tylercasson.com>
2023-12-02 00:54:04 -0600

    feat(day-02): add solution for part 1

---
+++ day-02/src/part2.rs
@@ -0,0 +1,15 @@
+use std::error::Error;
+
+pub fn run(input: &str) -> Result<String, Box<dyn Error>> {
+    todo!("Implement part 2");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::run;
+
+    #[test]
+    fn part2_example_input() {
+        todo!("Test example input for part 2");
+    }
+}
