d7ae9fdff94ea07c9c650384590c2eb2d641ed15
Tyler Casson <me@tylercasson.com>
2023-12-02 02:14:05 -0600

    chore(template): add basic divan benchmarking

---
+++ day-01/benches/benchmark.rs
@@ -0,0 +1,17 @@
+use day_01::*;
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench]
+fn part1() {
+    let input = divan::black_box(include_str!("../input/input1.txt"));
+    part1::run(input).expect("should benchmark part 1");
+}
+
+#[divan::bench]
+fn part2() {
+    let input = divan::black_box(include_str!("../input/input1.txt"));
+    part2::run(input).expect("should benchmark part 2");
+}
