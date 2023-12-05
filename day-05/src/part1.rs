1e7509f54972a29912f24d16ed6438b8817f61fa
Tyler Casson <me@tylercasson.com>
2023-12-05 00:29:51 -0600

    feat(day-05): initialize day 5

---
+++ day-05/src/part1.rs
@@ -0,0 +1,19 @@
+use std::error::Error;
+
+pub fn run(input: &str) -> Result<String, Box<dyn Error>> {
+    todo!("Implement part 1");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::run;
+
+    #[test]
+    fn part1_example_input() {
+        let input = "";
+
+        let expected = "".to_string();
+
+        assert_eq!(expected, run(input).expect("should return expected value"));
+    }
+}
