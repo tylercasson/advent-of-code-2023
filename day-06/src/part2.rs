558139304cb9dd4c5f21f18b27d7b1d57ca0e89f
Tyler Casson <me@tylercasson.com>
2023-12-06 01:09:33 -0600

    feat(day-06): add part 2 solution

--- day-06/src/part2.rs
+++ day-06/src/part2.rs
@@ -1,7 +1,54 @@
 use std::error::Error;
 
+#[derive(Debug)]
+pub struct Race {
+    time: f64,
+    dist: f64,
+}
+
+impl Race {
+    pub fn winning_options(&self) -> f64 {
+        // dist = time * x - x^2
+        let a = -1.0;
+        let b = self.time;
+        let c = -(self.dist + 1.0);
+        let disc = b * b - 4.0 * a * c;
+
+        let x1 = (-b + f64::sqrt(disc)) / (2.0 * a);
+        let x2 = (-b - f64::sqrt(disc)) / (2.0 * a);
+
+        // inclusive
+        x2.floor() - x1.ceil() + 1.0
+    }
+}
+
 pub fn run(input: &str) -> Result<String, Box<dyn Error>> {
-    todo!("Implement part 2");
+    let mut lines = input.lines();
+    let time: f64 = lines
+        .next()
+        .unwrap()
+        .split(':')
+        .last()
+        .unwrap()
+        .replace(' ', "")
+        .parse::<f64>()
+        .unwrap();
+
+    let dist: f64 = lines
+        .next()
+        .unwrap()
+        .split(':')
+        .last()
+        .unwrap()
+        .replace(' ', "")
+        .parse::<f64>()
+        .unwrap();
+
+    let race = Race { time, dist };
+
+    let options: f64 = race.winning_options();
+
+    Ok((options as u64).to_string())
 }
 
 #[cfg(test)]
@@ -9,10 +56,11 @@ mod tests {
     use super::run;
 
     #[test]
-    fn part2_example_input() {
-        let input = "";
+    fn example_input() {
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
 
-        let expected = "".to_string();
+        let expected = "71503".to_string();
 
         assert_eq!(expected, run(input).expect("should return expected value"));
     }
