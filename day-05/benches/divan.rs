bed055a100e94c7e660f0cf9e2df5bc30057d5a2
Tyler Casson <me@tylercasson.com>
2023-12-05 18:32:19 -0600

    refactor(day-05): disable benchmark for awful part 2

--- day-05/benches/divan.rs
+++ day-05/benches/divan.rs
@@ -10,8 +10,8 @@ fn part1() {
     part1::run(input).expect("should benchmark part 1");
 }
 
-#[divan::bench]
-fn part2() {
-    let input = divan::black_box(include_str!("../input/input1.txt"));
-    part2::run(input).expect("should benchmark part 2");
-}
+// #[divan::bench]
+// fn part2() {
+//     let input = divan::black_box(include_str!("../input/input1.txt"));
+//     part2::run(input).expect("should benchmark part 2");
+// }
