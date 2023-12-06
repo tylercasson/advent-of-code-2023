6ab9e6a87c90ad3da95a0230b9888a882172c5a2
Tyler Casson <me@tylercasson.com>
2023-12-06 00:55:04 -0600

    feat(day-06): add part 1 solution

--- day-06/src/part1.rs
+++ day-06/src/part1.rs
@@ -1,7 +1,51 @@
-use std::error::Error;
+use std::{error::Error, iter::zip};
+
+#[derive(Debug)]
+pub struct Race {
+    time: f32,
+    dist: f32,
+}
 
 pub fn run(input: &str) -> Result<String, Box<dyn Error>> {
-    todo!("Implement part 1");
+    let mut lines = input.lines();
+    let times: Vec<f32> = lines
+        .next()
+        .unwrap()
+        .split(' ')
+        .filter(|el| !el.is_empty())
+        .flat_map(|time| time.parse::<f32>())
+        .collect();
+
+    let dists: Vec<f32> = lines
+        .next()
+        .unwrap()
+        .split(' ')
+        .filter(|el| !el.is_empty())
+        .flat_map(|dist| dist.parse::<f32>())
+        .collect();
+
+    let races: Vec<Race> = zip(times, dists)
+        .map(|(time, dist)| Race { time, dist })
+        .collect();
+
+    let options: f32 = races
+        .iter()
+        .map(|race| {
+            // dist = time * x - x^2
+            let a = -1.0;
+            let b = race.time;
+            let c = -(race.dist + 1.0);
+            let disc = b * b - 4.0 * a * c;
+
+            let x1 = (-b + f32::sqrt(disc)) / (2.0 * a);
+            let x2 = (-b - f32::sqrt(disc)) / (2.0 * a);
+
+            // inclusive
+            x2.floor() - x1.ceil() + 1.0
+        })
+        .product();
+
+    Ok((options as u32).to_string())
 }
 
 #[cfg(test)]
@@ -9,10 +53,11 @@ mod tests {
     use super::run;
 
     #[test]
-    fn part1_example_input() {
-        let input = "";
+    fn example_input() {
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
 
-        let expected = "".to_string();
+        let expected = "288".to_string();
 
         assert_eq!(expected, run(input).expect("should return expected value"));
     }
