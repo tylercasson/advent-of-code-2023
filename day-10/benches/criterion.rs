2206b8dd17ab7aa9bc7a04d21115536698a566d1
Tyler Casson <me@tylercasson.com>
2023-12-12 16:22:20 -0600

    revert: "chore(day-10): limit part 2 benchmarks"
    This reverts commit 85e41fdd2453ca547816ac57a0335bee4834c114.

--- day-10/benches/criterion.rs
+++ day-10/benches/criterion.rs
@@ -10,14 +10,9 @@ pub fn part1(c: &mut Criterion) {
 
 pub fn part2(c: &mut Criterion) {
     let input = black_box(include_str!("../input/input1.txt"));
-    let mut group = c.benchmark_group("slow part2");
-
-    group.sample_size(10);
-    group.bench_function("day_10::part2", |b| {
+    c.bench_function("day_10::part2", |b| {
         b.iter(|| part2::run(input).expect("should benchmark part2"))
     });
-
-    group.finish();
 }
 
 criterion_group!(benches, part1, part2);
