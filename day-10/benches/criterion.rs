85e41fdd2453ca547816ac57a0335bee4834c114
Tyler Casson <me@tylercasson.com>
2023-12-12 15:31:00 -0600

    chore(day-10): limit part 2 benchmarks

--- day-10/benches/criterion.rs
+++ day-10/benches/criterion.rs
@@ -10,9 +10,14 @@ pub fn part1(c: &mut Criterion) {
 
 pub fn part2(c: &mut Criterion) {
     let input = black_box(include_str!("../input/input1.txt"));
-    c.bench_function("day_10::part2", |b| {
+    let mut group = c.benchmark_group("slow part2");
+
+    group.sample_size(10);
+    group.bench_function("day_10::part2", |b| {
         b.iter(|| part2::run(input).expect("should benchmark part2"))
     });
+
+    group.finish();
 }
 
 criterion_group!(benches, part1, part2);
